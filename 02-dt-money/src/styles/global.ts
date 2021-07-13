206579cbf84e8a8d2d2e87a7678745d962228500
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-07-12 22:54:18 -0300

    feat(aula02):Componente : Summary

--- 02-dt-money/src/styles/global.ts
+++ 02-dt-money/src/styles/global.ts
@@ -3,7 +3,8 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #F0F2F5;
-    --red : #E52E4D;
+    --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
