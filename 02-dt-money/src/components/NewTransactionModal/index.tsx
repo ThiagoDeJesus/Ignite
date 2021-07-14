18646377b938ab24fa5c77b3b7eb4324a3507a3d
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-07-14 11:09:47 -0300

    feat(aula02): Criando botoes de tipo + instalaçao da biblioteca polished

--- 02-dt-money/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal';
+import outcomeImg from '../../assets/outcome.svg';
+
+import incomeImg from '../../assets/income.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -34,7 +37,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           placeholder="valor"
         />
 
-        
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Entrada" />
+            <span>Saída</span>
+          </button>
+
+        </TransactionTypeContainer>
 
         <input
           placeholder="Categoria"
