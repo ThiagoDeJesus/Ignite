c54cba2fca8579971f80f3aad3959b004adf17ea
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-07-14 21:17:11 -0300

    feat(aula02): Movendo criaçao de transactions para o context

--- 02-dt-money/src/TransactionsContext.tsx
+++ 02-dt-money/src/TransactionsContext.tsx
@@ -10,22 +10,35 @@ interface Transaction {
   createdAt: string;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
-
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export function TransactionsProvider({children}: TransactionsProviderProps){
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
-  
+
   useEffect(() => {
     api.get('transactions')
-    .then(response => setTransactions(response.data.transactions))
+      .then(response => setTransactions(response.data.transactions))
   }, []);
 
+
+
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={ { transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   )
