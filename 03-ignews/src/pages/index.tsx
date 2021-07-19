0e306392ddda83e2367c7d95b9dd9bf76bfe7b01
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-07-19 20:04:55 -0300

    feat(aula03): Alterando de SSR para SSG

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe'
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JF4spEBPu4vbJgNxiKtgVCh')
   
   const product = {
@@ -51,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
