500c9a536a3f3a182befd6a08bd1f7a58cc67711
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-07-22 11:45:19 -0300

    feat(aula02): Gerando sessao de checkout

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +6,23 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }) {
+  const [session] = useSession();
+  
+  function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    // criação da checkout sessionario
+    
+  }
+  
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
