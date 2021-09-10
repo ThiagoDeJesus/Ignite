887027287acd59822316b71fe5f69bc93ab20fb9
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-09-10 15:45:37 -0300

    feat: estrutura da aplicaçao

--- 03-ignews/src/pages/_app.tsx
+++ 03-ignews/src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import { AppProps } from 'next/app';
-import { Header } from '../components/Header';
-import { Provider as NextAuthProvider } from 'next-auth/client';
+import { AppProps } from "next/app";
+import Head from 'next/head';
 
-import '../styles/global.scss';
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider session={pageProps.session}>
-      <Header />
+    <>
+      <Head>
+        {" "}
+        <title>ig.news</title>
+      </Head>
       <Component {...pageProps} />
-    </NextAuthProvider>
-  )
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
